3f2fa45bcc08c09d389745579a1c4174aa4cbf1b
Mohamed ABDALLAH <medaby@outlook.fr>
2025-04-09 17:07:46 +0200

    maj article repository #3

--- devbook-api/src/main/java/co/simplon/devbookapi/repositories/ArticleRepository.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/repositories/ArticleRepository.java
@@ -1,11 +1,15 @@
 package co.simplon.devbookapi.repositories;
 
 import co.simplon.devbookapi.entities.Article;
+import co.simplon.devbookapi.entities.RssProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     boolean existsByUrl(String url);
+    Set<Article> findByProviderOrderByIdAsc(RssProvider provider);
 }
