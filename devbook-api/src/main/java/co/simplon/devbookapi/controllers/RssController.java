1c8c1fc430847336048354e315adfe7cceacc1a7
Mohamed ABDALLAH <medaby@outlook.fr>
2025-04-09 17:04:17 +0200

    add rss controller #3

--- devbook-api/src/main/java/co/simplon/devbookapi/controllers/RssController.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/controllers/RssController.java
@@ -1,4 +1,27 @@
 package co.simplon.devbookapi.controllers;
 
+import co.simplon.devbookapi.entities.RssProvider;
+import co.simplon.devbookapi.services.RssService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/rss")
 public class RssController {
+     private final RssService rssService;
+
+     public RssController(RssService rssService) {
+         this.rssService = rssService;
+     }
+
+     @PostMapping("/import")
+     public ResponseEntity<RssProvider> importFeed(@RequestParam String url) throws IOException {
+         RssProvider provider = rssService.fetchAndStoreFrom(url);
+         return ResponseEntity.ok(provider);
+     }
 }
