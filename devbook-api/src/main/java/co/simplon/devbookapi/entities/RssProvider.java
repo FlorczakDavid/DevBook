728cc13423fd07b5988990414ddf94b4ce21b1dc
Mohamed ABDALLAH <medaby@outlook.fr>
2025-04-09 17:05:03 +0200

    add rss provider entity #3

--- devbook-api/src/main/java/co/simplon/devbookapi/entities/RssProvider.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/entities/RssProvider.java
@@ -1,4 +1,67 @@
 package co.simplon.devbookapi.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_rss_providers")
 public class RssProvider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String subtitle;
+    private String link;
+    private ZonedDateTime lastUpdate;
+
+//    @OneToMany(mappedBy = "provider", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    @JsonManagedReference
+//    @OrderBy("id ASC")
+//    private Set<Article> items;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ZonedDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+    public void setLastUpdate(ZonedDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+//
+//    public Set<Article> getItems() {
+//        return items;
+//    }
+//
+//    public void setItems(Set<Article> items) {
+//        this.items = items;
+//    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
 }
