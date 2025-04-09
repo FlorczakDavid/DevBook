ca9a36cb1a73bb92050da00f3497d8ba2623c3b3
Mohamed ABDALLAH <medaby@outlook.fr>
2025-04-09 17:05:57 +0200

    add rss provider repository #3

--- devbook-api/src/main/java/co/simplon/devbookapi/repositories/RssProviderRepository.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/repositories/RssProviderRepository.java
@@ -1,4 +1,10 @@
 package co.simplon.devbookapi.repositories;
 
-public interface RssProviderRepository {
+import co.simplon.devbookapi.entities.RssProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RssProviderRepository extends JpaRepository<RssProvider, Long> {
+    Optional<RssProvider> findByLink(String link);
 }
