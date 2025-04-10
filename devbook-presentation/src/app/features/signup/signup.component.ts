eb0abb7a2a170fc09ded624b192b4aeaa19ceb42
Émilie Barré <diavianne@gmail.com>
2025-04-10 22:24:05 +0200

    add form component + sign up component

---
+++ devbook-presentation/src/app/features/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormComponent } from '../../shared/form/form.component';
+
+@Component({
+  selector: 'app-signup',
+  imports: [FormComponent],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent {}
