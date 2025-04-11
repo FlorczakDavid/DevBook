38eb22478f2664b4ec636855de058bec724aa152
Mohamed ABDALLAH <medaby@outlook.fr>
2025-04-11 16:41:49 +0200

    merge maj signin signup

--- devbook-presentation/src/app/app.routes.ts
+++ devbook-presentation/src/app/app.routes.ts
@@ -1,15 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {SignInComponent} from './features/sign-in/sign-in.component'
-import { AuthPinComponent } from './features/auth-pin/auth-pin.component';
-import { NotFoundComponent } from './layouts/not-found/not-found.component';
+import {AuthPinComponent} from './features/auth-pin/auth-pin.component';
+import {NotFoundComponent} from './layouts/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: 'sign-in', component: SignInComponent },
-    { path: 'auth-pin/:token', component: AuthPinComponent },
   {
     path: 'profile',
     loadComponent: () => import('./layouts/profile/profile.component')
       .then(mod => mod.ProfileComponent)
   },
-  { path: '**', component: NotFoundComponent }
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'auth-pin/:token', component: AuthPinComponent},
+  {path: '**', component: NotFoundComponent}
 ];
