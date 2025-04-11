41a6b7b89fe00def62b98bc64e6baea614da72c8
David FLORCZAK <florczakdavid@gmail.com>
2025-04-11 09:19:59 +0200

    creation de la page 404 et de la page profile, ajout d'un router link vers la page de profile dans le header

--- devbook-presentation/src/app/app.routes.ts
+++ devbook-presentation/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile',
+    loadComponent: () => import('./layouts/profile/profile.component')
+      .then(mod => mod.ProfileComponent)
+  },
+  { path: '**', component: NotFoundComponent }
+];
