d9912b94417d2a5482e86e488948f56dad3b79a4
David FLORCZAK <florczakdavid@gmail.com>
2025-04-11 09:16:04 +0200

    creation gestionnaire notifications et affichage du nombre de notifications dans le front

--- devbook-presentation/src/app/features/header/notification/notification.component.ts
+++ devbook-presentation/src/app/features/header/notification/notification.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-
+import { fetchEventSource, EventSourceMessage } from '@microsoft/fetch-event-source';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-notification',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.css'
 })
 export class NotificationComponent {
   notifications: string[] = [];
+  abortController: AbortController = new AbortController()
+
+  ngOnInit() {
+    this.startListening();
+  }
+
+  startListening() {
+    fetchEventSource('http://localhost:8080/sse', {
+      method: 'GET',
+      onmessage: (ev: EventSourceMessage) => {
+        console.log(ev);
+        const gotJson = JSON.parse(ev.data);
+        console.log(gotJson);
+        this.notifications.push(gotJson.message);
+        console.log(this.notifications);
+      },
+      signal: this.abortController.signal,
+    });
+  }
+
+  ngOnDestroy() {
+    this.abortController.abort();
+  }
 }
--- devbook-presentation/src/app/layouts/header/header.component.ts
+++ devbook-presentation/src/app/layouts/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProfileLinkComponent } from '../../features/header/profile-link/profile-link.component';
 import { NotificationComponent } from '../../features/header/notification/notification.component';
-
 @Component({
   selector: 'app-header',
-  imports: [NotificationComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    NotificationComponent,
+    ProfileLinkComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  //
+  // showNotification: boolean = localStorage.getItem('role') == 'MEMBER';
+  showNotification: boolean = true; //pour tester
 }
