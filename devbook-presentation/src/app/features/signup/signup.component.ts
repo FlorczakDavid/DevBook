e2dc50ae35c34bcd73b476eb089e94fe8eb5dfdf
Émilie Barré <diavianne@gmail.com>
2025-04-11 02:03:59 +0200

    update validation form

--- devbook-presentation/src/app/features/signup/signup.component.ts
+++ devbook-presentation/src/app/features/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../../shared/form/form.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,19 @@ import { FormComponent } from '../../shared/form/form.component';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signup.valid) {
+      console.log('Form Submitted!', this.signup.value);
+    }
+  }
+}
--- devbook-presentation/src/app/shared/form/form.component.ts
+++ devbook-presentation/src/app/shared/form/form.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
-export class FormComponent {}
+export class FormComponent {
+  @Input() formGroup: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  @Output() formSubmit = new EventEmitter<void>();
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value);
+      this.formSubmit.emit();
+    }
+  }
+  isInvalidAndTouchedOrDirty(control: any): boolean {
+    return control.invalid && (control.dirty || control.touched);
+  }
+}
