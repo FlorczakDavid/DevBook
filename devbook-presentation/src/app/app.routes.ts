7652d6c3e8601299bd500a0bef280ec27072eb4c
Émilie Barré <diavianne@gmail.com>
2025-04-12 19:41:51 +0200

    Update app.routes.ts

--- devbook-presentation/src/app/app.routes.ts
+++ devbook-presentation/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {SignInComponent} from './features/sign-in/sign-in.component'
+import { SignInComponent } from './features/sign-in/sign-in.component';
+import { SignupComponent } from './features/signup/signup.component';
 
 export const routes: Routes = [
-    { path: 'sign-in', component: SignInComponent }
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'signup', component: SignupComponent },
 ];
