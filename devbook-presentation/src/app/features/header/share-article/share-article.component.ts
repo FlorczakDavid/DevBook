bbea0242566fc2b8451e081f5f7f5ce2320ef2ee
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-05-06 13:37:03 +0200

    trying custom validator

--- devbook-presentation/src/app/features/header/share-article/share-article.component.ts
+++ devbook-presentation/src/app/features/header/share-article/share-article.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-share-article',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './share-article.component.html',
   styleUrl: './share-article.component.css'
 })
 export class ShareArticleComponent {
+  urlForm = new FormGroup({
+    url: new FormControl('', [
+      forbiddenUrlValidator()
+    ])
+  });
+  onSubmit(){
+    
+  }
 
 }
--- devbook-presentation/src/app/layouts/header/header.component.ts
+++ devbook-presentation/src/app/layouts/header/header.component.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ProfileLinkComponent } from '../../features/header/profile-link/profile-link.component';
 import { NotificationComponent } from '../../features/header/notification/notification.component';
+import { ShareArticleComponent } from "../../features/header/share-article/share-article.component";
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
     RouterLink,
     NotificationComponent,
-    ProfileLinkComponent
-  ],
+    ProfileLinkComponent,
+    ShareArticleComponent
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
