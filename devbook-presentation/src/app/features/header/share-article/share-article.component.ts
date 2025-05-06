3802de4ec3bcc290477a4576817992eb86dfbe2a
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-05-06 09:03:17 +0200

    url form

---
+++ devbook-presentation/src/app/features/header/share-article/share-article.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-article',
+  imports: [],
+  templateUrl: './share-article.component.html',
+  styleUrl: './share-article.component.css'
+})
+export class ShareArticleComponent {
+
+}
