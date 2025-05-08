fd5d932922e19c975cd611cc59398879d91ff3d5
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-05-08 13:46:22 +0200

    retrieving api from other branch

--- devbook-api/src/main/java/co/simplon/devbookapi/services/ArticleService.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/services/ArticleService.java
@@ -1,12 +1,13 @@
 package co.simplon.devbookapi.services;
 
 import co.simplon.devbookapi.dtos.ArticleCreate;
-import co.simplon.devbookapi.dtos.ArticleView;
+import co.simplon.devbookapi.dtos.ValidArticle;
 import co.simplon.devbookapi.entities.Article;
 import co.simplon.devbookapi.repositories.ArticleRepository;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,24 +21,18 @@ public class ArticleService {
         this.articleRepository = articleRepository;
     }
 
-    public ArticleView postArticle(ArticleCreate input) throws IOException {
+    public ResponseEntity<Object> postArticle(ArticleCreate input) throws IOException {
         try{
-            Article article = createArticle(input);
-            return new ArticleView(
-                    article.getUrl(),
-                    article.getTitle(),
-                    article.getImagePath(),
-                    article.getDescription(),
-                    article.getAuthor()
-            );
+            ValidArticle validatedArticle = validArticle(input);
+            createArticle(validatedArticle);
         }catch(IOException e){
-            System.out.println("Cannot access to the article");
+            System.out.println(e.getMessage());
         }
         return null;
     }
 
-    private Article createArticle(ArticleCreate input) throws IOException {
-        Document doc = Jsoup.connect(input.url()).get(); //https://www.mediapart.fr/
+    private ValidArticle validArticle(ArticleCreate input) throws IOException {
+        Document doc = Jsoup.connect(input.url()).get();
         Elements title = doc.select("meta[property='og:title']");
         String contentTitle = title.attr("content");
         Elements type = doc.select("meta[property='og:type']");
@@ -48,15 +43,29 @@ public class ArticleService {
         String contentUrl = url.attr("content");
         Elements description = doc.select("meta[property='og:description']");
         String contentDescription = description.attr("content");
-        Elements author = doc.select(String.format("meta[property='%s1:author']", contentType));
+        Elements author = doc.select(String.format("meta[property='%s1:authors']", contentType));
         String contentAuthor = author.attr("content");
 
+        return checkArticle(contentUrl, contentTitle, contentImage, contentDescription, contentAuthor);
+    }
+
+    private static ValidArticle checkArticle(String contentUrl, String contentTitle, String contentImage, String contentDescription, String contentAuthor) {
+        return new ValidArticle(
+                contentUrl,
+                contentTitle,
+                contentImage,
+                contentDescription,
+                contentAuthor
+        );
+    }
+
+    private void createArticle(ValidArticle articleValidated) {
         Article article = new Article();
-        article.setUrl(contentUrl);
-        article.setTitle(contentTitle);
-        article.setImagePath(contentImage);
-        article.setDescription(contentDescription);
-        article.setAuthor(contentAuthor);
-        return articleRepository.save(article);
+        article.setUrl(articleValidated.url());
+        article.setTitle(articleValidated.title());
+        article.setImagePath(articleValidated.imagePath());
+        article.setDescription(articleValidated.description());
+        article.setAuthors(articleValidated.authors());
+        articleRepository.save(article);
     }
 }
