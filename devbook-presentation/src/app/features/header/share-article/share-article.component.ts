6c403f78b248129d3d1db5bd7d0fc374f28667e0
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-05-08 12:45:09 +0200

    trying to fetch

--- devbook-presentation/src/app/features/header/share-article/share-article.component.ts
+++ devbook-presentation/src/app/features/header/share-article/share-article.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-share-article',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './share-article.component.html',
   styleUrl: './share-article.component.css'
 })
 export class ShareArticleComponent {
-  urlForm = new FormGroup({
-    url: new FormControl('', [
-      forbiddenUrlValidator()
-    ])
-  });
-  onSubmit(){
+  // urlForm = new FormGroup({
+  //   url: new FormControl('', [
+  //     forbiddenUrlValidator()
+  //   ])
+  // });
+  // onSubmit(){
     
-  }
+  // }
 
   formGroup = new FormGroup({
     url: new FormControl('', [
@@ -26,9 +27,28 @@ export class ShareArticleComponent {
     ]),
   });
 
-  onSubmit() {
+  async onSubmit() {
     if (this.formGroup.valid) {
       console.log(this.formGroup.value);
+      try {
+        const response = await fetch('http://localhost:8080/article', {
+          method: "POST",
+          headers : {"Content-type": "application/json"},
+          body: JSON.stringify({url: "url"})
+          
+        })
+      } catch(err:any) {
+        if(err.status.code >= 400 || err.status.code <= 500){
+          console.log('error', err)
+          alert('client error')
+        } else if(err.status.code <= 500 || err.status.code <= 600) {
+            console.log('err', err)
+            alert('server error')
+        } else {
+          console.log('err', err)
+          alert('an enexpected error has occured')
+        }
+      } 
     }
   }
 
