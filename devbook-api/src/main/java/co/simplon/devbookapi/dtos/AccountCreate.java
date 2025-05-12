1cbbac3cadfd4a1cd31e91abadfb1cb1c5d45eef
Émilie Barré <diavianne@gmail.com>
2025-05-12 15:39:39 +0200

    add validation unique email

--- devbook-api/src/main/java/co/simplon/devbookapi/dtos/AccountCreate.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/dtos/AccountCreate.java
@@ -1,9 +1,10 @@
 package co.simplon.devbookapi.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record AccountCreate(@NotBlank @Size(max = 225) String username, @NotBlank String password) {
+public record AccountCreate(@NotBlank @Email @Size(max = 255) String username, @NotBlank @Size(max=72) String password) {
 
     @Override
     public String toString() {
