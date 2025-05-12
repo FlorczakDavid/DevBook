804864ae495638c6c1c62e9a23920e3c99ddaf7b
Mohamed ABDALLAH <medaby@outlook.fr>
2025-05-12 16:11:58 +0200

    refact: front rss

--- devbook-presentation/src/app/features/rss-providers/rss-providers.component.ts
+++ devbook-presentation/src/app/features/rss-providers/rss-providers.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import {Component, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {RssProvidersPage} from '../../shared/components/forms/rss-providers.page';
+import {RssProvider} from '../../shared/components/@types/rss-provider';
 @Component({
   selector: 'app-rss-providers',
   imports: [],
@@ -7,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrl: './rss-providers.component.css'
 })
 export class RssProvidersComponent {
+  @ViewChild(RssProvidersPage) rssFormComponent!: RssProvidersPage;
 
+  rssProviderForm: FormGroup;
+  toastMessage: RssProvider | null  = null;
+
+  constructor(private readonly fb: FormBuilder, private readonly http: HttpClient) {
+    this.rssProviderForm = this.fb.group({
+      url: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.rssProviderForm.valid) {
+      console.log("valided form")
+      const formData = this.rssProviderForm.value;
+      console.log( 'Test composant rss Provider', formData);
+      this.http.post<RssProvider>('http://localhost:8080/rss/import', formData).subscribe({
+        next: (response)=> {
+          // Handle the response here
+          console.log('La demande a été envoyée avec succès !', response);
+          this.toastMessage = response  ;
+
+          // Appel de la méthode showToast du composant enfant
+          if (this.rssFormComponent) {
+            this.rssFormComponent.showToast(response);
+          }
+
+        } ,
+        error: (error) => {
+          console.error('Erreur d\'envoi', error);
+          if (this.rssFormComponent) {
+            this.rssFormComponent.showToastError(error.error);
+          }
+        }
+      })
+    } else {
+      console.log("Formulaire invalide :", this.rssProviderForm.errors);
+    }
+  }
 }
