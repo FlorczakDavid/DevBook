65e05e104e73a6609b1224775213e57a9c3b647f
Mohamed ABDALLAH <medaby@outlook.fr>
2025-05-12 15:04:30 +0200

    refactor: article repo

--- devbook-api/src/main/java/co/simplon/devbookapi/repositories/ArticleRepository.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/repositories/ArticleRepository.java
@@ -1,12 +1,10 @@
 package co.simplon.devbookapi.repositories;
 
 import co.simplon.devbookapi.entities.Article;
-import co.simplon.devbookapi.entities.RssProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
