9fa68b2044674c63e62d9bccacf3b282d55b316a
Mohamed ABDALLAH <medaby@outlook.fr>
2025-05-13 18:19:48 +0200

    add guard role

--- devbook-presentation/src/app/app.routes.ts
+++ devbook-presentation/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './features/signup/signup.component';
 import { AuthPinComponent } from './features/auth-pin/auth-pin.component';
 import { LandingPageComponent } from './layouts/landing-page/landing-page.component';
 import {accountGuard} from './core/guards/account.guard';
+import {roleGuard} from './core/guards/role.guard';
 export const routes: Routes = [
   { path: 'auth-pin/:token', component: AuthPinComponent },
   { path: '', component: LandingPageComponent },
@@ -12,7 +13,7 @@ export const routes: Routes = [
     path: 'rss-providers',
     loadComponent: () => import('./features/rss-providers/rss-providers.component')
       .then(mod => mod.RssProvidersComponent),
-    canActivate: [accountGuard]
+    canActivate: [roleGuard]
   },
   {
     path: 'profile',
