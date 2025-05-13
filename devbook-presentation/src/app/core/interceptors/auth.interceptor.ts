f61b34f5eb7d387ed7f11cbb00635d574feea8a4
Mohamed ABDALLAH <medaby@outlook.fr>
2025-05-13 14:56:09 +0200

    maj guard

---
+++ devbook-presentation/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+    const token = localStorage.getItem('token'); // Récupère le token depuis le localStorage
+    console.log('Intercepteur token:', token);
+    if (token) {
+        // Clone la requête et ajoute l'en-tête Authorization
+        const clonedReq = req.clone({
+            headers:
+                req.headers.append('Authorization', `Bearer ${token}`),
+        });
+        return next(clonedReq);
+    }
+
+    // Si aucun token, passe la requête sans modification
+    return next(req);
+};
