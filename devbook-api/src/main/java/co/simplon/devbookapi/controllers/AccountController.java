7b7583e7f362daf32d13efb31e3ddbfc69b95074
Émilie Barré <diavianne@gmail.com>
2025-05-14 14:16:39 +0200

    corrections conflits

--- devbook-api/src/main/java/co/simplon/devbookapi/controllers/AccountController.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/controllers/AccountController.java
@@ -27,6 +27,7 @@ import co.simplon.devbookapi.services.AccountService;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -39,16 +40,14 @@ public class AccountController {
     private final EmailConfirmationRepository emailConfirmationRepository;
     private final AccountRepository accountRepository;
     private final EmailConfirmationService emailConfirmationService;
- 
-    
-    
+
 
     public AccountController(AccountService service,
                              AccountAuthenticateService authService,
                              EmailConfirmationRepository emailConfirmationRepository,
-                             AccountRepository accountRepository, EmailConfirmationService emailConfirmationService
-         AccountRepository accountRepository, EmailConfirmationService emailConfirmationService) {
-                    
+                             EmailConfirmationService emailConfirmationService,
+                             AccountRepository accountRepository) {
+
         this.service = service;
         this.authService = authService;
         this.emailConfirmationRepository = emailConfirmationRepository;
@@ -59,8 +58,8 @@ public class AccountController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     void create(@RequestBody @Valid AccountCreate inputs) {
-       service.create(inputs);
-       
+        service.create(inputs);
+
     }
 
     @GetMapping
@@ -68,31 +67,31 @@ public class AccountController {
     String getAccount() {
         return service.getAccount();
     }
-    
+
     @PostMapping("/authenticate")
     @ResponseStatus(HttpStatus.ACCEPTED)
     void authentificate(@RequestBody Authentication inputs) {
         authService.authenticate(inputs);
     }
-    
+
     @PostMapping("/doubleAuth/{token}")
     @ResponseStatus(HttpStatus.CREATED)
     AuthInfo verifyPin(@PathVariable("token") String token, @RequestBody String pin) {
-    	return authService.verifyPin(pin, token);
+        return authService.verifyPin(pin, token);
     }
-    
+
     @GetMapping("/profile/{token}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     Object getProfile(@PathVariable("token") String token) {
-    	return service.getProfile(token);
+        return service.getProfile(token);
     }
-    
+
     @PatchMapping("/updateProfile")
     @ResponseStatus(HttpStatus.ACCEPTED)
     void updateProfile(@RequestBody ProfileUpdate inputs) {
-    	service.updateProfile(inputs);
+        service.updateProfile(inputs);
     }
-    
+
 
     @GetMapping("/confirm/{uuidToken}")
     public ResponseEntity<String> confirmEmail(@PathVariable String uuidToken) {
@@ -111,20 +110,5 @@ public class AccountController {
         return ResponseEntity.ok("eMail ok");
     }
 
-    @GetMapping("/confirm/{uuidToken}")
-    public ResponseEntity<String> confirmEmail(@PathVariable String uuidToken) {
-        EmailConfirmation emailConfirmation = emailConfirmationRepository.findByUuidToken(uuidToken)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Token perdu"));
 
-        if (emailConfirmation.getExpiration().isBefore(LocalDateTime.now())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token expiré");
-        }
-
-        Account account = emailConfirmation.getAccount();
-        account.setStatusEmail(true);
-        accountRepository.save(account);
-        emailConfirmationRepository.deleteByUuidToken(uuidToken);
-
-        return ResponseEntity.ok("eMail ok");
-    }
 }
--- devbook-api/src/main/java/co/simplon/devbookapi/entities/Account.java
+++ devbook-api/src/main/java/co/simplon/devbookapi/entities/Account.java
@@ -13,7 +13,7 @@ public class Account {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "username", unique = true, nullable = false)
+
     @Column(name = "username", unique = true, nullable = false)
     private String username;
 
